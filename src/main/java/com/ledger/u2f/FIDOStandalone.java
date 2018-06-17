630682d44962616c78401e39f8516249bb75b4fc
Teriame <richard_peter.mitev@stud.tu-darmstadt.de>
2018-06-17 19:04:29 +0200

    Made it work on 3.0.1 cards and switched to standard AID

--- src/main/java/com/ledger/u2f/FIDOStandalone.java
+++ src/main/java/com/ledger/u2f/FIDOStandalone.java
@@ -46,10 +46,10 @@ public class FIDOStandalone implements FIDOAPI {
         (ECPrivateKey) KeyBuilder.buildKey(KeyBuilder.TYPE_EC_FP_PRIVATE, KeyBuilder.LENGTH_EC_FP_256, false));
         Secp256r1.setCommonCurveParameters((ECKey) keyPair.getPrivate());
         Secp256r1.setCommonCurveParameters((ECKey) keyPair.getPublic());
-        random = RandomData.getInstance(RandomData.ALG_KEYGENERATION);
+        random = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
         // Initialize the unique wrapping key
         chipKey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_256, false);
-        random.nextBytes(scratch, (short) 0, (short) 32);
+        random.generateData(scratch, (short) 0, (short) 32);
         chipKey.setKey(scratch, (short) 0);
         cipherEncrypt = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
         cipherEncrypt.init(chipKey, Cipher.MODE_ENCRYPT, IV_ZERO_AES, (short) 0, (short) IV_ZERO_AES.length);
